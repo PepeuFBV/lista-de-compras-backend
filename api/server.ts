ebb83b4f0521871309b98a16c8aae0084e65e20b
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-16 19:53:01 -0300

    feat(cors): added cors checking

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,27 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import bodyParser from 'body-parser'
 
 dotenv.config()
 
 const app: Express = express()
+
+const allowedOrigins = [process.env.CLIENT_URL]
+const corsOptions = {
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+        console.log(origin)
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else if (!origin || origin === undefined) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 const port = process.env.PORT || 3002
 
 app.use(bodyParser.json())
