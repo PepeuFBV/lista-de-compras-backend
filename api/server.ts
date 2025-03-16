499ea82bc80b3d42a0b41f97779973fbd2d50cf0
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-16 18:42:39 -0300

    feat(vercel): vercel deployment setup

--- api/server.ts
+++ api/server.ts
@@ -1,13 +1,16 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3002
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server')
+    res.send('Express + TypeScript Server 12312')
 })
 
 app.listen(port, () => {
