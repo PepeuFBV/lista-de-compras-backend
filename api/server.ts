b227d027657f08bc7d0f8c3734581c2634695e48
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-16 19:07:12 -0300

    fix(vercel): fixed deployment settings

--- api/server.ts
+++ api/server.ts
@@ -1,13 +1,14 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
-import path from 'path'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3002
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server 12312')
